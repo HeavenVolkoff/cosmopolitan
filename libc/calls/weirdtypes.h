063a970282fa4df73664c3129fa7c737c0d4fba9
Vítor Vasconcellos <vasconcellos.dev@gmail.com>
2026-02-08 08:37:59 -0300

    Fix some types in weirdtypes.h to match what is used in some structs

--- libc/calls/weirdtypes.h
+++ libc/calls/weirdtypes.h
@@ -10,7 +10,7 @@ typedef uint8_t cc_t;
 typedef int64_t clock_t; /* uint64_t on xnu */
 typedef uint64_t dev_t;  /* int32_t on xnu */
 typedef uint64_t fsblkcnt_t;
-typedef int64_t fsfilcnt_t; /* uint32_t on xnu */
+typedef uint64_t fsfilcnt_t; /* uint32_t on xnu */
 typedef uint32_t gid_t;
 typedef int32_t id_t; /* int32_t on linux/freebsd/etc. */
 typedef uint32_t in_addr_t;
@@ -27,7 +27,7 @@ typedef int64_t register_t;
 typedef uint16_t sa_family_t; /* bsd:uint8_t */
 typedef uint32_t socklen_t;
 typedef uint32_t speed_t;
-typedef uint32_t suseconds_t;
+typedef int64_t suseconds_t;
 typedef uint64_t useconds_t;   /* uint32_t on xnu */
 typedef int64_t syscall_arg_t; /* uint64_t on xnu */
 typedef uint32_t tcflag_t;
@@ -38,13 +38,8 @@ typedef uint64_t rlim_t; /* int64_t on bsd */
 typedef int32_t clockid_t;
 typedef char *caddr_t;
 
-#ifdef __x86_64__
 typedef int64_t blksize_t; /* int32_t on xnu */
-typedef uint64_t nlink_t;
-#elif defined(__aarch64__)
-typedef int32_t blksize_t;
-typedef uint32_t nlink_t; /* uint16_t on xnu */
-#endif
+typedef uint64_t nlink_t; /* uint16_t on xnu */
 
 #define TIME_T_MAX __INT64_MAX__
 #define TIME_T_MIN (-TIME_T_MAX - 1)
