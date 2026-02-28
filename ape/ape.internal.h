4875a5b86442a96e6a32ff2e9a0ec190290b6dcc
Vítor Vasconcellos <vitor.vasconcellos@proton.me>
2026-02-28 04:34:22 -0300

    Revert disable APE_IS_SHELL_SCRIPT

--- ape/ape.internal.h
+++ ape/ape.internal.h
@@ -2,8 +2,8 @@
 #define COSMOPOLITAN_APE_APE_INTERNAL_H_
 #include "libc/dce.h"
 
-// #if SupportsWindows() || SupportsMetal() || SupportsXnu()
-// #define APE_IS_SHELL_SCRIPT
-// #endif
+#if SupportsWindows() || SupportsMetal() || SupportsXnu()
+#define APE_IS_SHELL_SCRIPT
+#endif
 
 #endif /* COSMOPOLITAN_APE_APE_INTERNAL_H_ */
